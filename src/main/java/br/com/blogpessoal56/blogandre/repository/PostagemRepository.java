f48d5c7e42b2e071cc25a3d92a6aa90474c7f99f
Andre Parelho <parelhoneves.andre@gmail.com>
2022-08-01 20:21:07 -0300

    Atualizando a classe PostagemRepository

--- src/main/java/br/com/blogpessoal56/blogandre/repository/PostagemRepository.java
+++ src/main/java/br/com/blogpessoal56/blogandre/repository/PostagemRepository.java
@@ -2,10 +2,13 @@ package br.com.blogpessoal56.blogandre.repository;
 
 import br.com.blogpessoal56.blogandre.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
