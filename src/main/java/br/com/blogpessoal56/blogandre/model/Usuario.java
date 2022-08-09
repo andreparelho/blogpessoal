e8ab1dd7c749eae0b8c9e82a0c06169b23ade5ed
Andre <parelhoneves.andre@gmail.com>
2022-08-09 11:45:57 -0300

    atualização da model usuario, colocando get and set et, laÃções onetomany e obriagatoriedade e tamanhos dos atributos

--- src/main/java/br/com/blogpessoal56/blogandre/model/Usuario.java
+++ src/main/java/br/com/blogpessoal56/blogandre/model/Usuario.java
@@ -4,6 +4,10 @@ package br.com.blogpessoal56.blogandre.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -14,14 +18,25 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull(message = "O atributo Nome é Obrigatório!")
     private String nome;
+
+    @NotNull(message = "O atributo Usuário é Obrigatório!")
+    @Email(message = "O atributo Usuário deve ser um email válido!")
     private String usuario;
+
+    @NotBlank(message = "O atributo Senha é Obrigatório!")
+    @Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
     private String senha;
+
+    @Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
     private String foto;
 
-    @OneToMany (mappedBy = "tema", cascade = CascadeType.REMOVE)
-    @JsonIgnoreProperties("tema")
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
     private List<Postagem> postagem;
+
+
 
     public long getId() {
         return id;
