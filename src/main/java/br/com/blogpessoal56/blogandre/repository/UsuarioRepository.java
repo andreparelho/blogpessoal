ad93f1c0245eda4834718d6c84ec65688dd4c3b6
Andre <parelhoneves.andre@gmail.com>
2022-08-09 11:10:08 -0300

    adicição da interface UsuarioRepository. Metodos de busca por Nome e Usuario.

---
+++ src/main/java/br/com/blogpessoal56/blogandre/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package br.com.blogpessoal56.blogandre.repository;
+
+import br.com.blogpessoal56.blogandre.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+    Optional<Usuario> findByUsuario(String usuario);
+}
